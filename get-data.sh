63b8509212a9c9a424bf593c00a803ceb496742a
asakhno <sakhno.ad18@physics.msu.ru>
2023-10-13 13:30:08 +0300

    coles_frozen_pretrain

---
+++ get-data.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# the same as get_data2.sh
+#mkdir data
+
+curl -OL https://storage.yandexcloud.net/ds-ods/files/materials/f2fadc4d/public_train.pqt data/
+
+#unzip competition_data_final_pqt.zip -d data/
+#mv alfabattle2b-boosters.pro.zip data/
+
+#mv data/train_transactions_contest data/train_transactions_contest.parquet
+#mv data/test_transactions_contest data/test_transactions_contest.parquet
